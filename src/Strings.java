a69a54d9b5981d29a3d862e9c3c06e817d9c9546
VioletViola <vivian.v18723@gmail.com>
2021-08-15 18:45:15 +0100

    Practice with escape character sequences.

--- src/Strings.java
+++ src/Strings.java
@@ -1,10 +1,14 @@
 public class Strings {
     public static void main(String[] args) {
-        System.out.println("This is a string!");
-        System.out.println('7');
-        System.out.println("Hello\tWorld!");
         System.out.println("\"\\u0061\" is " + "\u0061");
         System.out.println("\"\\u0062\" is " + "\u0062");
         System.out.println("\"\\u0063\" is " + "\u0063");
+        System.out.println('7');
+        System.out.println("H\rello World!");
+        System.out.println("Hello\tWorld!");
+        System.out.println("Hello\tWorld!".length());
+        System.out.println("Hello\nWorld!".length());
+        System.out.println("Hello World!".length());
+        System.out.println("This is a string!");
     }
 }
