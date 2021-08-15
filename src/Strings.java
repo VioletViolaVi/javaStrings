4504442f8f0889c2688a58e461723b338cf1539e
VioletViola <vivian.v18723@gmail.com>
2021-08-15 18:32:34 +0100

    Practice strings with java.

---
+++ src/Strings.java
@@ -0,0 +1,10 @@
+public class Strings {
+    public static void main(String[] args) {
+        System.out.println("This is a string!");
+        System.out.println('7');
+        System.out.println("Hello\tWorld!");
+        System.out.println("\"\\u0061\" is " + "\u0061");
+        System.out.println("\"\\u0062\" is " + "\u0062");
+        System.out.println("\"\\u0063\" is " + "\u0063");
+    }
+}
